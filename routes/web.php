19b7afff0bd13c3114533c1c49c2cc209c0d5697
VinuraJL <cb009513@students.apiit.lk>
2023-06-08 02:31:55 +0530

    Hotel view in home page and show 4 items ina  row, implemented the grid

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -26,3 +22,17 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+Route::get('hotel/{id}', function($id){
+
+    return view('hotel.show',[
+        'hotel'=>$id
+    ]);
+
+});
+
+
+Route::get('/', function () {
+    return view('home');
+})-> name('home');
